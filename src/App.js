01fab2a61b4fe6e79ce01a3c02a764492d21bd0a
DarkedgeTH <darkedgeth@gmail.com>
2022-03-23 14:48:18 -0400

    set up project and created App to work with

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import GifList from './components/GifList';
+import GifForm from './components/GifForm';
+import data from './data/gifs';
 
-function App() {
+function App(props) {
+  const gifs = data
+  const loading = false
+  const error = ''
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search for Gifs</h1>
+      <GifForm />
+
+      {
+        loading ? <h3>We are loading</h3> : <GifList gifs={gifs} />
+      }
+
     </div>
   );
 }
---
+++ src/data/gifs.js
@@ -0,0 +1,53 @@
+const data = [
+    {
+        type: "gif",
+        id: "tSjoI5BPjXsHzqQuCS",
+        url: "https://giphy.com/gifs/viralhog-viral-hog-tango-the-pup-works-hard-on-toy-tractor-tSjoI5BPjXsHzqQuCS",
+        username: "viralhog",
+        source: "https://viralhog.com",
+        title: "Dog GIF by ViralHog",
+        rating: "g",
+        import_datetime: "2021-05-11 23:30:58",
+        trending_datetime: "0000-00-00 00:00:00",
+        images: {
+            original: {
+                url: "https://media4.giphy.com/media/tSjoI5BPjXsHzqQuCS/giphy.gif?cid=80bc7db2scyiawq5ad73vqwgip7hehi9rntax4wkfvjj45s6&rid=giphy.gif&ct=g",
+                hash: "663ec7411a4c770d74922804412e3d48"
+            }
+        }
+    },{
+        type: "gif",
+        id: "q5DwPxGm6kQnJ9dz9H",
+        url: "https://giphy.com/gifs/viralhog-viral-hog-golden-retriever-gets-face-full-of-fresh-air-q5DwPxGm6kQnJ9dz9H",
+        username: "viralhog",
+        source: "https://viralhog.com",
+        title: "Dog GIF by ViralHog",
+        rating: "g",
+        import_datetime: "2021-05-19 15:10:25",
+        trending_datetime: "0000-00-00 00:00:00",
+        images: {
+            original: {
+                url: "https://media1.giphy.com/media/q5DwPxGm6kQnJ9dz9H/giphy.gif?cid=80bc7db2scyiawq5ad73vqwgip7hehi9rntax4wkfvjj45s6&rid=giphy.gif&ct=g",
+                hash: "9bd33f3d00782636f5059d57b30a21e4"
+            }
+        }
+    },{
+        type: "gif",
+        id: "HzkZeQM6aHcodITVXf",
+        url: "https://giphy.com/gifs/viralhog-viral-hog-synchronized-tail-wagging-HzkZeQM6aHcodITVXf",
+        username: "viralhog",
+        source: "https://viralhog.com",
+        title: "Dog GIF by ViralHog",
+        rating: "g",
+        import_datetime: "2021-04-13 23:41:31",
+        trending_datetime: "0000-00-00 00:00:00",
+        images: {
+            original: {
+                url: "https://media4.giphy.com/media/HzkZeQM6aHcodITVXf/giphy.gif?cid=80bc7db2scyiawq5ad73vqwgip7hehi9rntax4wkfvjj45s6&rid=giphy.gif&ct=g",
+                hash: "816caab2d44cfb628e11b2ca372a66be"
+            }
+        }
+    }
+]
+
+export default data;
