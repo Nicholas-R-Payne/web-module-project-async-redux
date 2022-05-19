1c6484a85301a41bf7e90771a9e5a961a6db8da8
DarkedgeTH <darkedgeth@gmail.com>
2022-05-19 10:45:42 -0400

    fixed search

--- src/components/GifForm.js
+++ src/components/GifForm.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { useState } from "react";
+import { getGifs } from "../actions";
 
-const GifForm = () => {
-    return (<form>
-        <input />
-        <button>Search</button>
-      </form>)
+const GifForm = props => {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleSubmit = (evt) => {
+      evt.preventDefault();
+      props.getGifs(searchTerm)
+    }
+
+    const handleChange = (evt) => {
+      setSearchTerm(evt.target.value)
+    }
+
+    return (<form onSubmit={handleSubmit}>
+      <input onChange={handleChange} />
+      <button>Search</button>
+    </form>)
 }
 
-export default GifForm;
+export default connect(null, {getGifs})(GifForm)
